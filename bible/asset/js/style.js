68eea5a4b539326cd4574308eabf6b7e4bbddd33
Niño Dacanay <dacanay@yahoo.com>
2015-10-23 20:18:44 +0800

    Niño Dacanay

---
+++ bible/asset/js/style.js
@@ -0,0 +1,195 @@
+$(document).ready(function(){
+    $('#genesisRead').click(function () {
+        $('#imageBible').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultgenesisRead').show();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusRead').hide();
+        $('#resultgenesisInfo').hide();
+        $('#resultgenesisW').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    });
+    $('#exodusRead').click(function () {
+        $('#imageBible').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultgenesisRead').hide();
+        $('#resultexodusRead').show();
+        $('#resultleviticusRead').hide();
+        $('#resultgenesisInfo').hide();
+        $('#resultgenesisW').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    });
+    $('#leviticusRead').click(function () {
+        $('#imageBible').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultleviticusRead').show();
+        $('#resultgenesisRead').hide();
+        $('#resultexodusRead').hide();
+        $('#resultgenesisInfo').hide();
+        $('#resultgenesisW').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    });
+    $('#numbersRead').click(function () {
+        $('#imageBible').hide();
+        $('#resultgenesisRead').hide();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusRead').hide();
+        $('#resultnumbersRead').show();
+        $('#resultgenesisInfo').hide();
+        $('#resultgenesisW').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    });
+
+
+
+    $('#genesisInfo').click(function () {
+        $('#imageBible').hide();
+        $('#resultgenesisRead').hide();
+        $('#resultgenesisInfo').show();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusRead').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultgenesisW').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    })
+
+    $('#exodusInfo').click(function () {
+        $('#imageBible').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultgenesisInfo').hide();
+        $('#resultexodusInfo').show();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusRead').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultgenesisW').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    })
+
+    $('#leviticusInfo').click(function () {
+        $('#imageBible').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultgenesisInfo').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusInfo').show();
+        $('#resultleviticusRead').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultgenesisW').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    })
+
+     $('#numbersInfo').click(function () {
+        $('#imageBible').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultgenesisInfo').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultleviticusRead').hide();
+        $('#resultnumbersInfo').show();
+        $('#resultnumbersRead').hide();
+        $('#resultgenesisW').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    })
+
+    $('#genesisW').click(function () {
+        $('#imageBible').hide();
+        $('#resultgenesisW').show();
+        $('#resultgenesisInfo').hide();
+        $('#resultgenesisRead').hide();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusRead').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    })
+
+    $('#exodusW').click(function () {
+        $('#imageBible').hide();
+        $('#resultgenesisW').hide();
+        $('#resultexodusW').show();
+        $('#resultgenesisInfo').hide();
+        $('#resultgenesisRead').hide();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusRead').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultleviticusW').hide();
+        $('#resultnumbersW').hide();
+    })
+
+    $('#leviticusW').click(function () {
+        $('#imageBible').hide();
+        $('#resultgenesisW').hide();
+        $('#resultexodusW').hide();
+        $('#resultleviticusW').show();
+        $('#resultgenesisInfo').hide();
+        $('#resultgenesisRead').hide();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusRead').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+        $('#resultnumbersW').hide();
+    })
+
+     $('#numbersW').click(function () {
+        $('#imageBible').hide();
+        $('#resultgenesisW').hide();
+        $('#resultexodusW').hide();
+        $('#resultnumbersW').show();
+        $('#resultleviticusW').hide();
+        $('#resultgenesisInfo').hide();
+        $('#resultgenesisRead').hide();
+        $('#resultexodusRead').hide();
+        $('#resultleviticusRead').hide();
+        $('#resultnumbersRead').hide();
+        $('#resultexodusInfo').hide();
+        $('#resultleviticusInfo').hide();
+        $('#resultnumbersInfo').hide();
+    })
+
+
+
+});
